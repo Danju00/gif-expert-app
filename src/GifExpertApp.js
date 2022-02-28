46619a6f0e049647e0f46e11290b43b83ef848e9
Daniel Gonzalez <leinad9265@gmail.com>
2022-02-28 00:50:06 -0600

    72. Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    
     const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
+    //const handleAdd = () => {
      //setCategories(['HunterXHunter', ...categories]);
-     setCategories(cats => [...cats, 'HunterXhunter']);
-    }
+     //setCategories(cats => [...cats, 'HunterXhunter']);
+     //}
+
 
   return (
     <>
         <h2>GifExpertApp</h2>
+        <AddCategory/>
         <hr/>
-        <button onClick={ handleAdd }>Agregar</button>
+       
 
         <ol>
             {
